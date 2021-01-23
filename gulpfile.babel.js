bc150872ca9c0f68aa5f87fcee1e29c6de1a9421
Alexander <hello@alexander.works>
2021-01-23 22:53:19 +0200

    Add webserver and image optimization

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,18 @@
 import gulp from 'gulp';
 import gpug from 'gulp-pug';
 import del from 'del';
+import ws from 'gulp-webserver';
+import image from 'gulp-image';
 
 const routes = {
   pug: {
+    watch: 'src/**/*.pug',
     src: 'src/*.pug',
     dest: 'build',
+  },
+  img: {
+    src: 'src/img/*',
+    dest: 'build/img',
   },
 };
 
@@ -14,7 +21,18 @@ const pug = () =>
 
 const clean = () => del([routes.pug.dest]);
 
-const prepare = gulp.series([clean]);
-const compile = gulp.series([pug]);
+const webserver = () =>
+  gulp.src(routes.pug.dest).pipe(ws({ livereload: true, open: true }));
 
-export const dev = gulp.series([prepare, compile]);
+const img = () =>
+  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+};
+
+const prepare = gulp.series([clean, img]);
+const compile = gulp.series([pug]);
+const run = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([prepare, compile, run]);
