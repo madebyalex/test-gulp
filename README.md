db58e253acdf8452181ee75a148805e43f8109fb
Alexander <hello@alexander.works>
2021-02-15 15:20:16 +0200

    Update 2021-02-15T13:20:14.001Z

--- README.md
+++
@@ -1,3 +0,0 @@
-# Gulp.js Test Project
-
-Learning gulp.js by following the crash course by Nicolás Serrano Arévalo (Nomad Coders).
--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-import { random } from './util';
-
-const random1 = random(25);
-const random2 = random(17);
-
-console.log(`${random1} & ${random2}`);
